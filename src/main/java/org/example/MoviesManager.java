c9ca136a6793ee755e34ea4866b1b693fdb62830
Aleksey Lipin <nat4eg95@gmail.com>
2023-05-22 11:29:44 +0400

    Final commit

--- src/main/java/org/example/MoviesManager.java
+++ src/main/java/org/example/MoviesManager.java
@@ -1,36 +1,40 @@
 package org.example;
 
 public class MoviesManager {
-    private String [] movies = new String[0];
+    private String[] movies = new String[0];
     private int limit;
-    public MoviesManager () {
-    this.limit = 5;
+
+    public MoviesManager() {
+        this.limit = 5;
     }
-    public MoviesManager (int limit) {
-    this.limit = limit;
+
+    public MoviesManager(int limit) {
+        this.limit = limit;
     }
-    public void addMovie (String movie) {
-    String [] tmp = new String[movies.length + 1];
-    for (int i = 0; i< movies.length; i++) {
-        tmp [i] = movies [i];
+
+    public void addMovie(String movie) {
+        String[] tmp = new String[movies.length + 1];
+        for (int i = 0; i < movies.length; i++) {
+            tmp[i] = movies[i];
+        }
+        tmp[tmp.length - 1] = movie;
+        movies = tmp;
     }
-    tmp [tmp.length - 1] = movie;
-    movies = tmp;
+
+    public String[] findAll() {
+        return movies;
     }
-    public String [] findAll () {
-    return movies;
-    }
-    public String [] findLast () {
+
+    public String[] findLast() {
         int resultlength;
-        if (movies.length <limit) {
+        if (movies.length < limit) {
             resultlength = movies.length;
+        } else {
+            resultlength = limit;
         }
-        else {
-          resultlength = limit;
-        }
-        String [] tmp = new String[resultlength];
-        for (int i =0; i< tmp.length; i++) {
-           tmp [i] = movies [movies.length - 1 - i];
+        String[] tmp = new String[resultlength];
+        for (int i = 0; i < tmp.length; i++) {
+            tmp[i] = movies[movies.length - 1 - i];
         }
         return tmp;
     }
--- src/test/java/MoviesManagerTest.java
+++ src/test/java/MoviesManagerTest.java
@@ -5,40 +5,42 @@ import org.junit.jupiter.api.Test;
 public class MoviesManagerTest {
 
     @Test
-    public void FindFilms () {
+    public void FindFilms() {
         MoviesManager manager = new MoviesManager();
 
-        String [] expected = {};
-       String[] actual = manager.findAll();
-        Assertions.assertArrayEquals(expected, actual);
-    }
-
-    @Test
-    public void ShowFilms () {
-        MoviesManager manager = new MoviesManager();
-
-        manager.addMovie("Bladshot");
-        manager.addMovie("Forward");
-        manager.addMovie("Hotel Belgrad");
-        manager.addMovie("Gentlemens");
-        String [] expected = {"Bladshot","Forward","Hotel Belgrad","Gentlemens"};
+        String[] expected = {};
         String[] actual = manager.findAll();
         Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
-    public void ShowFilmsConversely () {
+    public void ShowFilms() {
         MoviesManager manager = new MoviesManager();
 
         manager.addMovie("Bladshot");
         manager.addMovie("Forward");
         manager.addMovie("Hotel Belgrad");
         manager.addMovie("Gentlemens");
-        String [] expected = {"Gentlemens", "Hotel Belgrad", "Forward","Bladshot"};
+        String[] expected = {"Bladshot", "Forward", "Hotel Belgrad", "Gentlemens"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void ShowFilmsConversely() {
+        MoviesManager manager = new MoviesManager();
+
+        manager.addMovie("Bladshot");
+        manager.addMovie("Forward");
+        manager.addMovie("Hotel Belgrad");
+        manager.addMovie("Gentlemens");
+        String[] expected = {"Gentlemens", "Hotel Belgrad", "Forward", "Bladshot"};
         String[] actual = manager.findLast();
         Assertions.assertArrayEquals(expected, actual);
     }
+
     @Test
-    public void ShowFilmsOverLimit () {
+    public void ShowFilmsOverLimit() {
         MoviesManager manager = new MoviesManager();
 
         manager.addMovie("Bladshot");
@@ -47,7 +49,7 @@ public class MoviesManagerTest {
         manager.addMovie("Gentlemens");
         manager.addMovie("Invisible Man");
         manager.addMovie("Trolls");
-        String [] expected = {"Trolls", "Invisible Man","Gentlemens", "Hotel Belgrad", "Forward"};
+        String[] expected = {"Trolls", "Invisible Man", "Gentlemens", "Hotel Belgrad", "Forward"};
         String[] actual = manager.findLast();
         Assertions.assertArrayEquals(expected, actual);
     }
